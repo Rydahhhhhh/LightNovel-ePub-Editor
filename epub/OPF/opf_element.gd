ba16452f7972c068d9318f6cc887ac240eb10425
Rydahhhhhh <rmswerth@gmail.com>
2024-10-26 20:17:55 +1300

    Added traverse method

--- epub/OPF/opf_element.gd
+++ epub/OPF/opf_element.gd
@@ -7,6 +7,22 @@ func _init(_node: XMLNode) -> void:
 # ====================================================== #
 #                        METHODS                         #
 # ====================================================== #
+## Traverses the element tree starting from itself. [param depth] is the maximum depth to traverse. 
+## If [param depth] is -1, the entire tree will be traversed. A [param depth] of 0 will return only 
+## the initial element, while a [param depth] of 1 will include the initial element and its 
+## immediate children. Higher depths continue to include each subsequent level of descendants 
+## (e.g., [param depth=2] includes the initial element, its children, and its children's children).
+func traverse(depth: int = 1) -> Array[OpfElement]:
+	assert(depth >= -1)
+	
+	var elements: Array[OpfElement] = [self]
+	if depth != 0:
+		var new_depth = -1 if depth == -1 else depth - 1
+		for child: OpfElement in self.children:
+			elements.append_array(child.traverse_tree(depth - 1))
+	return elements
+
+
 # ====================================================== #
 #                      END OF FILE                       #
 # ====================================================== #
