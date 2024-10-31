8e645996c263239a24917a60f445969ee5e95312
Rydahhhhhh <rmswerth@gmail.com>
2024-11-01 10:45:32 +1300

    Base classes

--- epub/OPF/epub_property.gd
+++ epub/OPF/epub_property.gd
@@ -16,51 +16,75 @@ func _get_value():
 func _set_value(to):
 	return
 
+# ====================================================== #
+#                      BASE CLASSES                      #
+# ====================================================== #
 class ContentProperty extends EpubProperty:
-	## Property that derives its value from the elements content attribute.
+	## Property that derives its value from the nodes content attribute.
 	## Used in Epub 2.x format, as opposed to the preferred Epub 3.x
 	## format hich uses text value instead
 	func _get_value():
-		return self.element.text
-
-	func _set_value(to):
-		self.element.text = to 
-
-class TextProperty extends EpubProperty:
-	## Property that derives its value from the elements text.
-	## Used in Epub 3.x format. See, [ContentProperty] for further documentation.
-	func _get_value():
-		return self.element.attrs["content"]
+		return self.node.attributes["content"]
 	
 	func _set_value(to):
-		self.element.attrs["content"] = to
+		self.node.attributes["content"] = to
+
+class TextProperty extends EpubProperty:
+	## Property that derives its value from the nodes text.
+	## Used in Epub 3.x format. See, [ContentProperty] for further documentation.
+	func _get_value():
+		return self.node.text_content
+
+	func _set_value(to):
+		self.node.text_content = to 
 	
 class DoubleProperty extends EpubProperty:
 	## In scenarios where both the Epub 2.x & 3.x cannot be condensed into a 
-	## single element, we'll need elements for both versions
-	var _element_2x: OpfGeneralElement
-	var _element_3x: OpfGeneralElement
+	## single node, we'll need nodes for both versions
+	var _node_2x: XMLTree
+	var _node_3x: XMLTree
 	var _default_value
 	
 	func _get_value():
-		# Since we'll be making sure both the 2.x & 3.x elements will have the same
-		# value it doesn't matter which element we'll get from, using the 3.x element
+		# Since we'll be making sure both the 2.x & 3.x nodes will have the same
+		# value it doesn't matter which node we'll get from, using the 3.x node
 		# because the .text syntax is preferable
-		return self._element_3x.text
+		return self._node_3x.text_content
 	
 	func _set_value(to):
-		self._element_2x.attrs["content"] = to
-		self._element_3x.text = to
+		if to == null:
+			to = ""
+		self._node_2x.attributes["content"] = to
+		self._node_3x.text_content = to
 	
-	func resolve():
-		var temp_val = self._element_3x.text
+	func resolve() -> void:
+		var temp_val = self._node_3x.text_content
 		if temp_val == null or (temp_val is String and temp_val.is_empty()):
-			temp_val = self._element_2x.attrs["content"]
+			temp_val = self._node_2x.attributes.get_or_add("content", _default_value)
 		if temp_val == null or (temp_val is String and temp_val.is_empty()):
 			temp_val = _default_value
 		assert(temp_val == null or temp_val is String)
 		self.value = temp_val
+
+class GroupProperty extends EpubProperty:
+	## When a property has a cardinality of zero or more (creators, collections, etc..)
 	
+	var epub_properties: Array[EpubProperty] = []
+	
+	func _get_value():
+		var props = epub_properties.duplicate()
+		props.make_read_only()
+		return props
+	
+	func _get(property: StringName) -> Variant:
+		for epub_prop in self.epub_properties:
+			if epub_prop.value == property:
+				return epub_prop
+		return
+	
+	func _to_string() -> String:
+		return str(self.epub_properties)
+
 # ====================================================== #
 #                       INHERITORS                       #
 # ====================================================== #
